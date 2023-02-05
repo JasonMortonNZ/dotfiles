acf2670438c8a4c30d1b7f640a427076b9aa6ea7
Jason Morton <JasonMortonNZ@users.noreply.github.com>
2023-02-05 21:13:01 +1300

    Update install.sh

--- .dotfiles/bin/install.sh
+++ .dotfiles/bin/install.sh
@@ -1,4 +1,4 @@
-git clone --bare git@bitbucket.org:JasonMortonNZ/dotfiles.git $HOME/.dotfiles
+git clone --bare git@github.com:JasonMortonNZ/dotfiles.git $HOME/.dotfiles
 function dotfiles {
    /usr/local/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
 }
